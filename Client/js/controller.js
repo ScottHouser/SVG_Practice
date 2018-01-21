cec68ab64378fd5e1f58a4a62786eccc5b036ca9
ScottHouser <ScottAnthonyHouser@gmail.com>
2018-01-21 16:42:10 -0500

    first commit

---
+++ Client/js/controller.js
@@ -0,0 +1,21 @@
+app.controller('MainController', ['$scope', function($scope) { 
+  
+  $scope.title = 'Buy Poisonous Spiders.com'; 
+  $scope.titleElab = 'Your one stop shop for bulk poisonous spider purchaces'; 
+  $scope.link1 = 'Shop Spiders'; 
+  $scope.link2 = 'Spider Blog'; 
+  $scope.link3 = 'Venom by the liter'; 
+  $scope.link4 = 'Contact Us'; 
+  $scope.link5 = 'List of emergency toxicologists by reigon'; 
+  $scope.caption = 'You\'ll need a World Wide Web to hold all these Spiders!'; 
+  $scope.bottomCaption = ''; 
+  
+  
+  
+}]);/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
